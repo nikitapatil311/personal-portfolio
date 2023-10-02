29f8dc8ccb1a3f16083d89d6bc864963ff49b789
Nikita S Patil <nikitaspatil311@gmail.com>
2023-10-02 02:36:07 +0200

    css

--- js/main.js
+++ js/main.js
@@ -70,8 +70,8 @@ tabs.forEach((tab) => {
 
 /*==================== SERVICES MODAL ====================*/
 const modalViews = document.querySelectorAll(".services__modal"),
-const  modalBtns = document.querySelectorAll(".services__button"),
-const modalCloses = document.querySelectorAll(".services__modal-close");
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
 
 let modal = function (modalClick) {
   modalViews[modalClick].classList.add("active-modal");
